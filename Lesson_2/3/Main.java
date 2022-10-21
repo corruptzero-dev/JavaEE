23a38e3a1a8c7ce59b843181eaa5768c76811540
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2022-10-21 23:38:19 +0300

    Update Main.java

---
+++ Lesson_2/3/Main.java
@@ -0,0 +1,20 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        SomeClass clazz = new SomeClass("text", 21);
+        try{
+            String json = mapper.writeValueAsString(clazz);
+            System.out.println(json);
+            mapper.writeValue(new File("src/main/resources/clazz.json"), clazz);
+            SomeClass clazz2 = mapper.readValue(new File("src/main/resources/clazz.json"), SomeClass.class);
+        } catch (Exception e) {
+            log.error("object mapper exception" + e.getMessage());
+            System.out.println(e.getMessage());
+        }
+    }
+}
