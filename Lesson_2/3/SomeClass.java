c2b0ad0a4810c281492fe66e375b2349913e49bc
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2022-10-21 23:39:57 +0300

    edited

--- Lesson_2/3/SomeClass.java
+++ Lesson_2/3/SomeClass.java
@@ -1,20 +1,25 @@
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.extern.slf4j.Slf4j;
+public class SomeClass {
+    private String someStrAttr;
+    private Integer someIntAttr;
 
-import java.io.File;
-@Slf4j
-public class Main {
-    public static void main(String[] args) {
-        ObjectMapper mapper = new ObjectMapper();
-        SomeClass clazz = new SomeClass("text", 21);
-        try{
-            String json = mapper.writeValueAsString(clazz);
-            System.out.println(json);
-            mapper.writeValue(new File("src/main/resources/clazz.json"), clazz);
-            SomeClass clazz2 = mapper.readValue(new File("src/main/resources/clazz.json"), SomeClass.class);
-        } catch (Exception e) {
-            log.error("object mapper exception" + e.getMessage());
-            System.out.println(e.getMessage());
-        }
+    public SomeClass(String s, int i) {
+        this.someStrAttr = s;
+        this.someIntAttr = i;
+    }
+
+    public String getSomeStrAttr() {
+        return someStrAttr;
+    }
+
+    public void setSomeStrAttr(String someStrAttr) {
+        this.someStrAttr = someStrAttr;
+    }
+
+    public Integer getSomeIntAttr() {
+        return someIntAttr;
+    }
+
+    public void setSomeIntAttr(Integer someIntAttr) {
+        this.someIntAttr = someIntAttr;
     }
 }
