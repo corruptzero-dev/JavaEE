980aea6bc5a6052c2d3c63ba05e762e0c113bec6
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2022-10-21 23:40:40 +0300

    edited

--- Lesson_2/3/SomeClass.java
+++ Lesson_2/3/SomeClass.java
@@ -1,25 +1,11 @@
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class SomeClass {
     private String someStrAttr;
     private Integer someIntAttr;
-
-    public SomeClass(String s, int i) {
-        this.someStrAttr = s;
-        this.someIntAttr = i;
-    }
-
-    public String getSomeStrAttr() {
-        return someStrAttr;
-    }
-
-    public void setSomeStrAttr(String someStrAttr) {
-        this.someStrAttr = someStrAttr;
-    }
-
-    public Integer getSomeIntAttr() {
-        return someIntAttr;
-    }
-
-    public void setSomeIntAttr(Integer someIntAttr) {
-        this.someIntAttr = someIntAttr;
-    }
 }
